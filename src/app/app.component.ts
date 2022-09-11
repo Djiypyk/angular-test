07c8ec2b4ed596c0825d8fb9353d19878a69764b
Zarazaka Mikalai <golmtz@gmail.com>
2022-09-12 00:12:41 +0300

    add filter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,6 +15,7 @@ export class AppComponent implements OnInit {
   products$: Observable<IProduct[]>
 
   loading = false
+  term = ''
 
   constructor(private productsService: ProductsService) {
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,27 @@
 import {HttpClientModule} from "@angular/common/http";
 import {NgModule} from '@angular/core';
+import {FormsModule} from "@angular/forms";
 import {BrowserModule} from '@angular/platform-browser';
 import {ProductComponent} from "src/app/components/product/product.component";
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import { GlobalErrorComponent } from './components/global-error/global-error.component';
+import { FilterProductsPipe } from './pipes/filter-products.pipe';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProductComponent,
-    GlobalErrorComponent
+    GlobalErrorComponent,
+    FilterProductsPipe
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
-  ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
